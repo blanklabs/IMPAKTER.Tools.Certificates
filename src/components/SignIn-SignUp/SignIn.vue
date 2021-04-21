61c2006077b4843a68c776e18efc8edf6b383726
anlsim <anlsimental@gmail.com>
2021-04-21 11:32:46 -0700

    Adding SignIn and SignUp Containers

---
+++ src/components/SignIn-SignUp/SignIn.vue
@@ -0,0 +1,105 @@
+<template>
+  <b-container fluid="md" id="signin_main">
+    <img id="impakterLogo" src="@/assets/logo_index.png" />
+    <h1 >Sign In to Index Certificate Tool</h1>
+    <p>We suggest sin gth emeail address you use at work.</p>
+    <b-button id="GoogleButton" @click="login">
+      <img src="@/assets/google_logo.png" id="googleLogo"/> Sign in with Google
+    </b-button>
+    <div class="separator">
+      <hr><h5>OR</h5><hr>
+    </div>
+    <b-form-input id="identifier"
+        v-model="identifier"
+        placeholder="name@work-email.com"
+        required>
+    </b-form-input>
+   
+    <b-button @click="login" id="signInButton">Sign in with Email</b-button>
+    <hr>
+    <p>Don’t have an account? <a href="#">Sign Up</a></p>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "SignIn",
+};
+
+</script>
+
+<style scoped>
+h1{
+  padding: 10px;
+  font-weight: bold;
+}
+
+hr{
+  flex: 1;
+  height: 1px;
+  background-color: #BBBBBB;
+}
+
+a:link {
+  font-weight: bold;
+  color: black;
+  text-decoration: none;
+}
+
+#signin_main{
+  margin-top: 10px;
+  max-width: 500px;
+}
+
+#impakterLogo{
+  margin-left:60px;
+  padding: 10px;
+  max-width: 300px;
+}
+
+#googleLogo{
+  display: inline-block;
+  margin-right: 15px;
+  max-width: 25px;
+}
+
+ #GoogleButton{
+    display: inline-block;
+    background: white;
+    color: #0F993E;
+    width: 100%;
+    border-radius: 5px;
+    border: thin solid #0F993E;
+    white-space: nowrap;
+    font-weight: bold;
+ }
+
+
+  #signInButton{
+    display: inline-block;
+    background: white;
+    color: #1D2029;
+    width: 100%;
+    border-radius: 5px;
+    border: 2px solid #989898;
+    white-space: nowrap;
+    font-weight: bold;
+  }
+
+  .separator{
+    margin-top: 10px;
+    margin-bottom: 10px;
+    display: flex;
+    align-items: center;
+  }
+
+  .separator h5{
+    padding: 0 2rem; /* creates the space */
+  }
+  #identifier{
+    margin-top: 10px;
+    margin-bottom: 10px;
+}
+
+
+</style>
---
+++ src/components/SignIn-SignUp/SignUp-Part2.vue
@@ -0,0 +1,96 @@
+<template>
+  <b-container fluid="md" id="signup_main">
+      <img src="@/assets/logo_index.png" id="impakterLogo"/>
+
+      <h1>Sign-up</h1>
+
+      <h4>Contact info</h4>
+        
+      <b-form-input
+          id="identifier"
+          v-model="identifier"
+          placeholder="Company Name"
+          required>
+      </b-form-input>
+       
+      <b-form-input
+          id="identifier"
+          v-model="identifier"
+          placeholder="Your Role"
+          required>
+      </b-form-input>
+
+      
+      <b-button @click="login" id="action_btt">Sign Up</b-button>
+      
+  </b-container>
+  
+</template>
+
+<script>
+export default {
+  name: "SignUp-Part2",
+  
+};
+</script>
+
+<style scoped>
+h1{
+  padding: 10px;
+  font-weight: bold;
+}
+
+hr{
+  flex: 1;
+  height: 1px;
+  background-color: #BBBBBB;
+}
+
+a:link {
+  font-weight: bold;
+  color: black;
+  text-decoration: none;
+}
+#identifier{
+    margin-top: 10px;
+    margin-bottom: 10px;
+}
+
+#signup_main{
+  margin-top: 10px;
+  max-width: 500px;
+}
+
+#impakterLogo{
+  margin-left:60px;
+  padding: 10px;
+  max-width: 300px;
+}
+
+
+  #action_btt{
+    display: inline-block;
+    background: white;
+    color: #1D2029;
+    width: 100%;
+    border-radius: 5px;
+    border: 2px solid #989898;
+    white-space: nowrap;
+    font-weight: bold;
+  }
+
+  .separator{
+    padding: 10px;
+    display: flex;
+    align-items: center;
+  }
+
+  .separator h5{
+    padding: 0 2rem; /* creates the space */
+  }
+
+  #emailForm{
+    padding: 10px;
+  }
+
+</style>
