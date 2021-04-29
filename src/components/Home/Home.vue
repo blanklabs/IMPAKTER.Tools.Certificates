0998284a3531555aa8666c3fc312ecef1855df39
anlsim <anlsimental@gmail.com>
2021-04-28 19:13:06 -0700

    Adding components for Landing Page

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,38 +1,43 @@
 <template>
-<div>
-    <b-container id="Header">
-        <b-row>
-            <b-col>
-            <HeaderDescription headerTitle = "Impakter Certificate Tool">
-            Certificates description policies and actions.  We check their certifications, aggregated data, news and how they are tackling the UN 17 Sustainable Development Goals. 
-            </HeaderDescription> 
-            <ActionButton  btnIcon="caret-right" btnDescription="   Get Started"></ActionButton>
-            <ActionButton  btnIcon="person-circle" btnDescription="   Sign In"></ActionButton>
-            </b-col>
-            <b-col>
-            <HeaderImage></HeaderImage> 
-            </b-col>
-        </b-row>
-    </b-container>
 
-    <b-container>
-        <b-row>
-            <b-col><CardItem></CardItem></b-col>
-            <b-col><CardItem></CardItem></b-col>
-            <b-col><CardItem></CardItem></b-col>
+    <div class="fullscreen">
+        <NavLanding/>
+        <b-row id="header">
+            <b-col>
+              <HeaderDescription headerTitle = "Impakter Certificate Tool">
+                We analyze how sustainable the Top 100 Global Brands 2020 are. We analyze how sustainable the Top 100 Global Brands 2020 are.
+              </HeaderDescription> 
+                <ActionButton  btnIcon="chevron-right" btnDescription="   Get Started"></ActionButton>
+                <ActionButton  btnIcon="person-circle" btnDescription="   Sign In"></ActionButton>
+          
+            </b-col>
+            <b-col>
+            <img id="headerImage" src="../../assets/banner-newdpi.png"/>
+            </b-col>
+      </b-row>
+    
+        <b-row id="smallCards">
+            <b-col><CardItem cardIcon="folder2-open" cardTitle="Sample Title" iconColor="#8EC184">We analyze how sustainable the Top 100 Global Brands 2020 are.</CardItem></b-col>
+            <b-col><CardItem cardIcon="folder2-open" cardTitle="Sample Title" iconColor="#E2CB2C">We analyze how sustainable the Top 100 Global Brands 2020 are.</CardItem></b-col>
+            <b-col><CardItem cardIcon="folder2-open" cardTitle="Sample Title" iconColor="#F79755">We analyze how sustainable the Top 100 Global Brands 2020 are.</CardItem></b-col>
+            <b-col><CardItem cardIcon="folder2-open" cardTitle="Sample Title" iconColor="#2880C3">We analyze how sustainable the Top 100 Global Brands 2020 are.</CardItem></b-col>
         </b-row>
         
-    </b-container>
-    <b-container>
-         <b-row>
-            <b-col><CardItemLarge></CardItemLarge></b-col>
-            <b-col><CardItemLarge></CardItemLarge></b-col>
+         <b-row id="bigCards">
+            <b-col><CardItemLarge cardIcon="building" cardTitle="Large Sample Title">We analyze how sustainable the Top 100 Global Brands 2020 are.</CardItemLarge></b-col>
+            <b-col><CardItemLarge cardIcon="building" cardTitle="Large Sample Title">We analyze how sustainable the Top 100 Global Brands 2020 are.</CardItemLarge></b-col>
         </b-row>
-    </b-container>
+        <div>
+          <img src="@/assets/banner-newdpi.png" id="imgImpact"/>
+        </div>
+        <div>
+          <h3>Our Partners</h3>
+          <img src="@/assets/Partners_Landing.png" id="partners"/>
+        </div>     
+        <Footer/>
+    </div>
    
-   
-   
-</div>
+
 
 
 </template>
@@ -40,10 +45,11 @@
 <script>
 import {
     HeaderDescription, 
-    HeaderImage, 
     CardItem,
     CardItemLarge,
     ActionButton } from "uicomponents"
+import Footer from "./Footer"
+import NavLanding from "./NavLanding"
 
 export default {
 name: "Home",
@@ -55,10 +61,11 @@ data(){
 },
 components:{
      HeaderDescription,
-     HeaderImage,
      CardItem,
      CardItemLarge,
-     ActionButton
+     ActionButton,
+     Footer,
+     NavLanding
      }
 
 
@@ -67,5 +74,43 @@ components:{
 
 </script>
 <style scoped>
+.fullscreen { 
+  font-family: 'Montserrat', sans-serif;
+  position: absolute;
+  top: 100px;
+  left: 100px;
+  right: 100px;
+}
 
+div{
+  padding: 15px;
+}
+
+
+ #header{
+   min-height: 300px;
+   padding: 20px;
+ }
+ #btnHrd{
+    display: flex;
+    flex-wrap: wrap;
+    align-content: center;
+ }
+ #smallCards{
+   padding: 20px;
+ }
+ #bigCards{
+   padding: 20px;
+ }
+ #headerImage{
+   max-width: 1200px;
+ }
+ 
+ #imgImpact{
+   width: 100%;
+ }
+ #partners{
+   max-width: 1200px;
+ }
+ 
 </style>
---
+++ src/components/Home/NavLanding.vue
@@ -0,0 +1,73 @@
+<template>
+ <div >
+    <b-navbar toggleable="lg" type="dark" fixed="top" id="nav">
+      <b-navbar-brand href="https://index.impakter.com">
+        <img id="logo" src="@/assets/logo_index.png"/>
+      </b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+
+        <!-- Right aligned nav items -->
+         <b-navbar-nav class="ml-auto">
+            <b-nav-item ><router-link to="/home">
+                  <span>HOME</span></router-link>
+              </b-nav-item>
+              <b-nav-item>
+                  <router-link to="#">ABOUT US</router-link>
+              </b-nav-item>
+              <b-nav-item href="#">
+                  <router-link to="#"> HOW IT WORKS</router-link>
+              </b-nav-item>
+              <b-nav-item href="#">
+                  <router-link to="#">CONTANCT US</router-link>
+              </b-nav-item>
+              <b-nav-item>
+                  <span v-if="loggedIn" @click="logout">SIGN-OUT</span>
+                  <span v-if="!loggedIn" @click="login">SIGN-IN</span>
+              </b-nav-item> 
+              <b-nav-item> | </b-nav-item> 
+               <b-nav-item href="#">
+                  <router-link to="#">INDEX</router-link>
+              </b-nav-item>
+                 <b-nav-item href="#">
+                  <router-link to="#">MARKETPLACE</router-link>
+              </b-nav-item>
+             
+                <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template #button-content>
+            <b-icon  icon="search" style="color: #EA5456"></b-icon>
+          </template>
+          <b-dropdown-item href="#"> <b-nav-form>
+                <b-form-input
+                  size="sm"
+                  class="mr-sm-2"
+                  placeholder="Search">
+                  </b-form-input>
+                <b-button type="submit"><b-icon  icon="search" style="color: #EA5456"></b-icon></b-button>
+              </b-nav-form>
+              </b-dropdown-item>
+        </b-nav-item-dropdown>
+
+      
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div> 
+
+
+</template>
+
+<script>
+export default {
+  name: "NavLanding",
+};
+</script>
+<style scoped>
+#nav{
+  font-family: Montserrat;
+  box-shadow: 0 2px 4px 0 rgba(0,0,0,.2);
+}
+</style>
