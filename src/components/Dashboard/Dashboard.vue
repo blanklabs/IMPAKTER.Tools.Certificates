1551378f4fc090a9b0c2586772f28d4f5acd752a
anlsim <anlsimental@gmail.com>
2021-05-04 12:16:05 -0700

    Adding Dashboard Content

--- src/components/Dashboard/Dashboard.vue
+++ src/components/Dashboard/Dashboard.vue
@@ -1,19 +1,27 @@
 <template>
-  <div>
+  <div id="dashBoard">
     <Sidebar />
     <div>
       <b-alert v-if="isSignupSuccess" variant="success" show dismissible fade
         >Profile updated successfully. Please verify your email address by
-        checking your inbox.</b-alert
-      >
-      <p>Main</p>
+        checking your inbox.</b-alert>
+        <p>Main</p>
     </div>
+     <img  id="headerImg" src="../../assets/LandingPage/dummyHeader.png"/>
+     <div id="headerSection">
+         <h1>Welcome Message</h1>
+            <ActionButton btnIcon="person-circle" btnDescription="Complete your Profile"/>
+            <ActionButton btnIcon="file-earmark-text" btnDescription="Fill your First Certificate"/>
+
+     </div>
+      
   </div>
 </template>
 <script>
+import { ActionButton } from "uicomponents"
 import Sidebar from "../Shared/Sidebar";
 export default {
-  name: "Footer",
+  name: "Dashboard",
   data() {
     return {
       isSignupSuccess: false,
@@ -21,6 +29,7 @@ export default {
   },
   components: {
     Sidebar,
+    ActionButton,
   },
   mounted() {
     this.isSignupSuccess = this.$store.getters["user/signupStatus"];
@@ -29,4 +38,17 @@ export default {
 </script>
 
 <style scoped>
+#dashBoard{
+    margin-left: 100px;
+    font-family: 'Montserrat', sans-serif;
+}
+#headerImg{
+    max-height: auto;
+    max-width: 1000px;
+}
+#headerSection{
+    align-content: center;
+    
+    padding: 20px;
+}
 </style>
--- src/components/Shared/Sidebar.vue
+++ src/components/Shared/Sidebar.vue
@@ -20,9 +20,9 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .sidenav {
- font-family: Montserrat;
+  font-family: 'Montserrat';
   height: 100%;
   width: 230px;
   position: fixed;
