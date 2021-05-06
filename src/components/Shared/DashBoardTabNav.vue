a86aca7c1b171fcbeb4b8f87bad65f7f8dc27dd2
anlsim <anlsimental@gmail.com>
2021-05-06 14:25:10 -0700

    Styles DashboardTabNav

--- src/components/Shared/DashBoardTabNav.vue
+++ src/components/Shared/DashBoardTabNav.vue
@@ -1,13 +1,14 @@
 <template>
 <div>
   <ul class="nav">
-   
     <li v-for="tab in tabs" :key="tab" class="nav-item">
       <a class="nav-link" :class="{active: tab == selected}" @click="setTab(tab)">
         {{tab}}
       </a>
     </li>
+    
   </ul>
+     <hr/>
   <slot></slot>
 </div>
 </template>
@@ -31,5 +32,36 @@ export default {
 };
 </script>
 <style scoped>
+*{
+  font-family: Montserrat;
+}
+ul{
+  margin-bottom: 0;
+  padding-bottom: 0;
+}
+a{
+  font-size: 18px;
+  color:#222222;
+  text-decoration: none;
+}
+a:hover{
+  font-weight: bolder;
+}
+.nav-item:hover {
+  border-bottom-style: solid;
+  border-bottom-color: #fe6663;
+}
 
+hr{
+  margin-top: 0;
+  padding-top: 0;
+  height: 3px;
+  background-color: #EBEBEB;
+  border: none;
+
+}
+/* hr{
+  height: 3px;
+  background-color: #EA5456;
+} */
 </style>
