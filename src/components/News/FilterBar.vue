1b2172e4004cc1ec468c7865fd91df88c828e1ba
anlsim <anlsimental@gmail.com>
2021-05-11 12:38:31 -0700

    Adding styles to filter bar

--- src/components/News/FilterBar.vue
+++ src/components/News/FilterBar.vue
@@ -1,29 +1,32 @@
 <template>
 
-    <b-form inline>
+    <b-form class="newsFilter" inline>
        
         <b-form-input
-          class="mb-2 mr-sm-2 mb-sm-0"
+          class="newsInput keywords"
           id="keywords"
           v-model="form.keywords"
           placeholder="Input Keywords..."
           required
         ></b-form-input>
         <b-form-datepicker
-          class="mb-2 mr-sm-2 mb-sm-0"
+          class="newsInput date"
           id="date"
           v-model="form.date"
           placeholder="Date"
           :options="dates"
         ></b-form-datepicker>
         <b-form-select
-          class="mb-2 mr-sm-2 mb-sm-0"
+          class="newsInput"
           id="publicationPriority"
           v-model="form.priority"
           
           :options="priorities"
-        ></b-form-select>
-         <b-button type="submit" variant="primary">Go</b-button>
+        ></b-form-select >
+        <b-button class="formBtn" 
+         type="submit" 
+         variant="primary">Go
+        </b-button>
    
       </b-form>
 
@@ -47,5 +50,22 @@ export default {
 }
 </script>
 <style scoped>
+.newsInput{
+  margin: auto;
+  border: 2px solid #BBBBBB;
+}
+.keywords{
+  min-width: 450px;
+}
+.date{
+  min-width: 200px;
+}
+.formBtn{
+  color: #BBBBBB;
+  font-weight: bold;
+  background: none;
+  border: 2px solid #BBBBBB;
+  border-radius: 5px;
 
+}
 </style>
