9f9ce7635d542e77356d2b9b86d80a6d9df93de2
anlsim <anlsimental@gmail.com>
2021-05-19 21:40:24 -0700

    Adding matches content

--- src/components/Matches/MatchesHome.vue
+++ src/components/Matches/MatchesHome.vue
@@ -1,6 +1,27 @@
 <template>
     <div>
-      Coming Soon 
+      <h1>Matches</h1>
+      <img class="headerImg" src="../../assets/LandingPage/dummyHeader.png" />
+        <b-row class="contentRow">
+          <b-col>
+            <p>Certificates description policies and actions.
+              We check their certifications, aggregated data, 
+              news and how they are tackling the UN 17 Sustainable
+              Development Goals. Certificates description policies and actions.
+              We check their certifications, aggregated data, 
+              news and how they are tackling the UN 17 Sustainable 
+              Development Goals. Certificates description policies and actions. 
+              We check their certifications, aggregated data,
+              news and how they are tackling the UN 17 Sustainable Development Goals. 
+          </p>
+          </b-col>
+          <b-col class="titleCol">
+             <h2>Coming Soon</h2>
+          </b-col>
+         
+          
+        </b-row>
+          
       
     </div>
 </template>
@@ -10,5 +31,26 @@ export default {
 };
 </script>
 <style scoped>
+* {
+  font-family: "Montserrat";
+  text-align: left;
+  align-items: center;
+}
 
+.headerImg {
+
+  margin-top: 10px;
+  max-width: 100%;
+}
+
+.row{
+  margin-top: 20px;
+}
+.col{
+  margin: 10px;
+}
+
+h2{
+  text-align: center;
+}
 </style>
